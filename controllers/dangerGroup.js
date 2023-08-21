16f93dfc3a1b8b7e6dc4e44f4b55248fd0d1bfaa
User8755 <110420113+User8755@users.noreply.github.com>
2023-08-22 01:20:49 +0500

    обработка ошибко для группы опасности

--- controllers/dangerGroup.js
+++ controllers/dangerGroup.js
@@ -12,5 +12,13 @@ module.exports.createDangerGroup = (req, res) => {
   dangerGroup
     .create({ label, dangerID })
     .then((item) => res.send(item))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      console.log(err), console.log(err.keyPattern.label);
+      if (err.code === 11000 && err.keyPattern.label === 1) {
+        res.status(409).send({ message: 'Не врено заполнено название' });
+      }
+      if (err.code === 11000 && err.keyPattern.dangerID === 1) {
+        res.status(409).send({ message: 'Не врено заполнено Id' });
+      }
+    });
 };
--- models/dangerGroup.js
+++ models/dangerGroup.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
+
 // группа опасности
 const dangerGroup = new mongoose.Schema({
   label: {
     type: String,
     required: true,
     minlength: 2,
+    unique: true
   },
   dangerID: {
     type: Number,
     required: true,
     minlength: 1,
+    unique: true
   },
 });
 
