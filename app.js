f2fecabd61c47471f9a259c1ca4ab8570b9232e1
User8755 <110420113+User8755@users.noreply.github.com>
2024-02-16 08:25:24 +0500

    fix err

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
   console.log(err);
   res.status(statusCode).send({
-    message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
+    message: statusCode === 500 ? 'На сервере произошла ошибка' : err,
   });
   next();
 });
